6504034e7433c2246ff33db72cdec5a9a18cccf5
Teerasej <teerasej@gmail.com>
2017-07-05 23:40:28 +0700

    Implement Face Detect API and show some attribute in new page

--- CameraApp/CameraAppPage.xaml.cs
+++ CameraApp/CameraAppPage.xaml.cs
@@ -1,10 +1,15 @@
-﻿using Plugin.Media;
+﻿using System.Linq;
+using Microsoft.ProjectOxford.Face;
+using Plugin.Media;
+using Plugin.Media.Abstractions;
 using Xamarin.Forms;
 
 namespace CameraApp
 {
     public partial class CameraAppPage : ContentPage
     {
+        private MediaFile targetfile;
+
         public CameraAppPage()
         {
             InitializeComponent();
@@ -12,60 +17,95 @@ namespace CameraApp
 
         async void Handle_Take_Photo_Clicked(object sender, System.EventArgs e)
         {
-			await CrossMedia.Current.Initialize();
+            await CrossMedia.Current.Initialize();
 
-			if (!CrossMedia.Current.IsCameraAvailable || !CrossMedia.Current.IsTakePhotoSupported)
-			{
-				await DisplayAlert("No Camera", ":( No camera available.", "OK");
-				return;
-			}
+            if (!CrossMedia.Current.IsCameraAvailable || !CrossMedia.Current.IsTakePhotoSupported)
+            {
+                await DisplayAlert("No Camera", ":( No camera available.", "OK");
+                return;
+            }
 
-			var file = await CrossMedia.Current.TakePhotoAsync(new Plugin.Media.Abstractions.StoreCameraMediaOptions
-			{
-				Directory = "Sample",
-				Name = "test.jpg"
-			});
+            var file = await CrossMedia.Current.TakePhotoAsync(new Plugin.Media.Abstractions.StoreCameraMediaOptions
+            {
+                Directory = "Sample",
+                Name = "test.jpg"
+            });
 
-			if (file == null)
-				return;
+            if (file == null)
+                return;
 
-			await DisplayAlert("File Location", file.Path, "OK");
+            await DisplayAlert("File Location", file.Path, "OK");
 
-			image.Source = ImageSource.FromStream(() =>
-			{
-				var stream = file.GetStream();
-				file.Dispose();
-				return stream;
-			});
+            this.targetfile = file;
 
-			
+            image.Source = ImageSource.FromStream(() =>
+            {
+                var stream = file.GetStream();
+                //file.Dispose();
+                return stream;
+            });
 
 
-		}
+
+
+        }
 
         async void Handle_Camera_Roll_Clicked(object sender, System.EventArgs e)
         {
-			await CrossMedia.Current.Initialize();
+            await CrossMedia.Current.Initialize();
 
             if (!CrossMedia.Current.IsPickPhotoSupported)
-			{
-				await DisplayAlert("Pick Photo not supported", ":( No pick photo supported", "OK");
-				return;
-			}
+            {
+                await DisplayAlert("Pick Photo not supported", ":( No pick photo supported", "OK");
+                return;
+            }
 
 
             var file = await CrossMedia.Current.PickPhotoAsync(new Plugin.Media.Abstractions.PickMediaOptions
-			{
-			});
+            {
+            });
 
-			if (file == null)
-				return;
+            if (file == null)
+                return;
 
-			await DisplayAlert("File Location", file.Path, "OK");
+            await DisplayAlert("File Location", file.Path, "OK");
 
-			
-			image.Source = ImageSource.FromFile(file.Path);
-			
-		}
+            this.targetfile = file;
+
+            image.Source = ImageSource.FromFile(file.Path);
+
+        }
+
+        async void Handle_Identify_Clicked(object sender, System.EventArgs e)
+        {
+            using (var stream = this.targetfile.GetStream())
+            {
+                var faceServiceClient = new FaceServiceClient("43ed00a8d83e4b6b9faafb2ea411c654", "https://westcentralus.api.cognitive.microsoft.com/face/v1.0");
+
+
+                var requiredFaceAttributes = new FaceAttributeType[] {
+                  FaceAttributeType.Age,
+                  FaceAttributeType.Gender,
+                  FaceAttributeType.Smile,
+                  FaceAttributeType.HeadPose,
+                  FaceAttributeType.Glasses,
+                    FaceAttributeType.Occlusion
+                  };
+
+                // Step 4a - Detect the faces in this photo.
+                var faces = await faceServiceClient.DetectAsync(stream, true, true, requiredFaceAttributes);
+
+                if (faces.Length > 0 )
+                {
+                    await Navigation.PushAsync(new FacePage(faces[0]));
+                }
+                else 
+                {
+                    await DisplayAlert("No Face Detected","It seems no face in the image...","OK");   
+                }
+
+
+            }
+        }
     }
 }
---
+++ CameraApp/FacePage.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.ProjectOxford.Face.Contract;
+using Xamarin.Forms;
+
+namespace CameraApp
+{
+    public partial class FacePage : ContentPage
+    {
+        private Face detectedFace;
+
+        public FacePage(Face face)
+        {
+            InitializeComponent();
+
+            this.detectedFace = face;
+
+            labelAge.Text = this.detectedFace.FaceAttributes.Age.ToString();
+            labelHeadPosePitch.Text = this.detectedFace.FaceAttributes.HeadPose.Pitch.ToString();
+            labelHeadPoseRoll.Text = this.detectedFace.FaceAttributes.HeadPose.Roll.ToString();
+            labelHeadPoseYaw.Text = this.detectedFace.FaceAttributes.HeadPose.Yaw.ToString();
+            labelSmile.Text = this.detectedFace.FaceAttributes.Smile.ToString();
+            labelGlasses.Text = this.detectedFace.FaceAttributes.Glasses.ToString();
+            labelOcclusionEyeOccluded.Text = this.detectedFace.FaceAttributes.Occlusion.EyeOccluded.ToString();
+            labelOcclusionForeheadOccluded.Text = this.detectedFace.FaceAttributes.Occlusion.ForeheadOccluded.ToString();
+            labelOcclusionMouthOccluded.Text = this.detectedFace.FaceAttributes.Occlusion.MouthOccluded.ToString();
+;
+        }
+    }
+}
