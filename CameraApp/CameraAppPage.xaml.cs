d51593de537e2c4db024ca22ed17cdc33fd6edf2
Teerasej <teerasej@gmail.com>
2017-07-05 21:33:08 +0700

    Finish take photo from camera. Don't forget to update all Nuget package, both PCL and Droid, or you get error

---
+++ CameraApp/CameraAppPage.xaml.cs
@@ -0,0 +1,53 @@
+﻿using Plugin.Media;
+using Xamarin.Forms;
+
+namespace CameraApp
+{
+    public partial class CameraAppPage : ContentPage
+    {
+        public CameraAppPage()
+        {
+            InitializeComponent();
+        }
+
+        async void Handle_Take_Photo_Clicked(object sender, System.EventArgs e)
+        {
+			await CrossMedia.Current.Initialize();
+
+			if (!CrossMedia.Current.IsCameraAvailable || !CrossMedia.Current.IsTakePhotoSupported)
+			{
+				await DisplayAlert("No Camera", ":( No camera available.", "OK");
+				return;
+			}
+
+			var file = await CrossMedia.Current.TakePhotoAsync(new Plugin.Media.Abstractions.StoreCameraMediaOptions
+			{
+				Directory = "Sample",
+				Name = "test.jpg"
+			});
+
+			if (file == null)
+				return;
+
+			await DisplayAlert("File Location", file.Path, "OK");
+
+			image.Source = ImageSource.FromStream(() =>
+			{
+				var stream = file.GetStream();
+				file.Dispose();
+				return stream;
+			});
+
+			//or:
+			//image.Source = ImageSource.FromFile(file.Path);
+			//image.Dispose();
+
+
+		}
+
+        void Handle_Camera_Roll_Clicked(object sender, System.EventArgs e)
+        {
+            
+        }
+    }
+}
