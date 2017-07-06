cb37bdc558bb8163dd1a02f2761d822063516912
Teerasej <teerasej@gmail.com>
2017-07-06 09:33:05 +0700

    Prepare to start

--- CameraApp/CameraAppPage.xaml.cs
+++ CameraApp/CameraAppPage.xaml.cs
@@ -3,69 +3,63 @@ using Xamarin.Forms;
 
 namespace CameraApp
 {
-    public partial class CameraAppPage : ContentPage
-    {
-        public CameraAppPage()
-        {
-            InitializeComponent();
-        }
+	public partial class CameraAppPage : ContentPage
+	{
+		public CameraAppPage()
+		{
+			InitializeComponent();
+		}
 
-        async void Handle_Take_Photo_Clicked(object sender, System.EventArgs e)
-        {
+		// Async
+		async void Handle_Take_Photo_Clicked(object sender, System.EventArgs e)
+		{
+			// Init
 			await CrossMedia.Current.Initialize();
 
-			if (!CrossMedia.Current.IsCameraAvailable || !CrossMedia.Current.IsTakePhotoSupported)
-			{
-				await DisplayAlert("No Camera", ":( No camera available.", "OK");
-				return;
-			}
 
-			var file = await CrossMedia.Current.TakePhotoAsync(new Plugin.Media.Abstractions.StoreCameraMediaOptions
-			{
-				Directory = "Sample",
-				Name = "test.jpg"
-			});
-
-			if (file == null)
-				return;
-
-			await DisplayAlert("File Location", file.Path, "OK");
-
-			image.Source = ImageSource.FromStream(() =>
-			{
-				var stream = file.GetStream();
-				file.Dispose();
-				return stream;
-			});
-
+			// Check Is-Camera-Available and Is-Take-Photo-Support?
 			
+
+			// get file from TakePhotoAsync with Plugin.Media.Abstractions.StoreCameraMediaOptions{ Directory & Name }
+			
+
+			// Check if file is null
+			
+
+			// Show file.Path in DisplayAlert
+			
+
+			// Use ImageSource.FromStream to show file in image.Source
+			
+
+
 
 
 		}
 
-        async void Handle_Camera_Roll_Clicked(object sender, System.EventArgs e)
-        {
+		async void Handle_Camera_Roll_Clicked(object sender, System.EventArgs e)
+		{
 			await CrossMedia.Current.Initialize();
 
-            if (!CrossMedia.Current.IsPickPhotoSupported)
+
+			if (!CrossMedia.Current.IsPickPhotoSupported)
 			{
 				await DisplayAlert("Pick Photo not supported", ":( No pick photo supported", "OK");
 				return;
 			}
 
-
-            var file = await CrossMedia.Current.PickPhotoAsync(new Plugin.Media.Abstractions.PickMediaOptions
-			{
-			});
-
-			if (file == null)
-				return;
-
-			await DisplayAlert("File Location", file.Path, "OK");
-
+			// get file from PickPhotoAsync with Plugin.Media.Abstractions.StoreCameraMediaOptions{  }
 			
-			image.Source = ImageSource.FromFile(file.Path);
+
+            // Check file null
 			
+
+			// Show file.Path
+			
+
+			// Use file.Path in Image.FromFile();
+			
+
 		}
-    }
+	}
 }
