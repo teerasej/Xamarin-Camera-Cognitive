56d490154d65c41d26393beb748f5437bf52f480
Teerasej <teerasej@gmail.com>
2017-07-06 09:31:28 +0700

    Add comment

--- CameraApp/CameraAppPage.xaml.cs
+++ CameraApp/CameraAppPage.xaml.cs
@@ -10,27 +10,35 @@ namespace CameraApp
             InitializeComponent();
         }
 
+        // Async
         async void Handle_Take_Photo_Clicked(object sender, System.EventArgs e)
         {
+            // Init
 			await CrossMedia.Current.Initialize();
 
+
+            // Check Is-Camera-Available and Is-Take-Photo-Support?
 			if (!CrossMedia.Current.IsCameraAvailable || !CrossMedia.Current.IsTakePhotoSupported)
 			{
 				await DisplayAlert("No Camera", ":( No camera available.", "OK");
 				return;
 			}
 
+            // get file from TakePhotoAsync with Plugin.Media.Abstractions.StoreCameraMediaOptions{ Directory & Name }
 			var file = await CrossMedia.Current.TakePhotoAsync(new Plugin.Media.Abstractions.StoreCameraMediaOptions
 			{
 				Directory = "Sample",
 				Name = "test.jpg"
 			});
 
+            // Check if file is null
 			if (file == null)
 				return;
 
+            // Show file.Path in DisplayAlert
 			await DisplayAlert("File Location", file.Path, "OK");
 
+            // Use ImageSource.FromStream to show file in image.Source
 			image.Source = ImageSource.FromStream(() =>
 			{
 				var stream = file.GetStream();
@@ -47,23 +55,26 @@ namespace CameraApp
         {
 			await CrossMedia.Current.Initialize();
 
+
             if (!CrossMedia.Current.IsPickPhotoSupported)
 			{
 				await DisplayAlert("Pick Photo not supported", ":( No pick photo supported", "OK");
 				return;
 			}
 
-
-            var file = await CrossMedia.Current.PickPhotoAsync(new Plugin.Media.Abstractions.PickMediaOptions
+			// get file from PickPhotoAsync with Plugin.Media.Abstractions.StoreCameraMediaOptions{  }
+			var file = await CrossMedia.Current.PickPhotoAsync(new Plugin.Media.Abstractions.PickMediaOptions
 			{
 			});
 
+            // Check file
 			if (file == null)
 				return;
 
+            // Show file.Path
 			await DisplayAlert("File Location", file.Path, "OK");
 
-			
+            // Use file.Path in Image.FromFile();
 			image.Source = ImageSource.FromFile(file.Path);
 			
 		}
